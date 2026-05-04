62fec6a899c2848b55778042b2fb51bb48d78137
Tafida <Tafidasani5@gmail.com>
2026-05-04 20:35:18 +0100

    fix: add basename for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router';
-import Login from './Pages/Login';
-import Signup from './Pages/Signup';
-import Dashbord from './Pages/Dashbord';
-import ProtectedRoute from './components/ProtectedRoute';
+// ... other imports
 
 function App() {
   return (
-    <Router>
+    // Update this line below!
+    <Router basename="/Auth-master">
       <Routes>
-        {/* 1. Redirect root to Login by default */}
         <Route path="/" element={<Navigate to="/login" />} />
-
-        {/* 2. Public Routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-
-        {/* 3. Private Route (Dashboard) */}
         <Route path="/dashboard" element={<ProtectedRoute><Dashbord /></ProtectedRoute>} />
-
-        {/* 4. Catch-all: redirect any unknown URL to login */}
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </Router>
